5054f204f21a9713430d01204b5a09e4d14f4867
pofateeva <fateeva_pv@bk.ru>
2025-03-06 23:40:46 +0300

    Финальная версия проекта, добавили добавление комплиментов

--- main.py
+++ main.py
@@ -15,10 +15,19 @@ compliments = [
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет')
+    bot.send_message(message.chat.id, 'Привет! Если хочешь получить комплимент напиши /compliment, если хочешь добавить новый комплимент, напиши /add_compliment!')
 
 @bot.message_handler(commands=["compliment"])
 def send_compliment(message):
     bot.send_message(message.chat.id, random.choice(compliments))
+
+@bot.message_handler(commands=["add_compliment"])
+def add_compliment(message):
+    bot.send_message(message.chat.id, "Напиши новый комплимент, который хочешь добавить.")
+    bot.register_next_step_handler(message, save_new_compliment)
+
+def save_new_compliment(message):
+        compliments.append(message.text)
+        bot.send_message(message.chat.id, "Комплимент добавлен! ❤️")
 
 bot.polling()
